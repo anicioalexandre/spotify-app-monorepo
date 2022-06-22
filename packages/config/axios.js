583d4eb7f9b5727daec0cd6d1c46eef37eba93fb
Alexandre Anicio <aa@tsl.io>
2022-06-21 21:52:24 -0300

    chore: change auth

--- packages/config/axios.js
+++ packages/config/axios.js
@@ -1,10 +1,47 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
+import qs from 'qs'
 
+const SPOTIFY_TOKEN_API = 'https://accounts.spotify.com/api/token'
+
+const getTokenEndpoint = async () => {
+  const headers = {
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    auth: {
+      username: 'c962f7eb22704c228ba8ae721a4675c5',
+      password: '279924f6228c43249af2d24bec32b46d',
+    },
+  }
+  const data = {
+    grant_type: 'client_credentials',
+  }
+
+  try {
+    const response = await axios.post(
+      SPOTIFY_TOKEN_API,
+      qs.stringify(data),
+      headers,
+    )
+    return Promise.resolve(response.data.access_token)
+  } catch (error) {
+    return Promise.reject(error)
+  }
+}
 const instance = axios.create()
 
-instance.interceptors.request.use((request) => {
-  const token = Cookies.get('spotify-token')
+instance.interceptors.request.use(async (request) => {
+  const tokenCookie = Cookies.get('spotify-token')
+  let tokenFetched = ''
+
+  if (!tokenCookie) {
+    tokenFetched = await getTokenEndpoint()
+    Cookies.set('spotify-token', tokenFetched)
+  }
+
+  const token = tokenCookie || tokenFetched
 
   if (token) {
     if (request.headers && !request.headers.Authorization) {
