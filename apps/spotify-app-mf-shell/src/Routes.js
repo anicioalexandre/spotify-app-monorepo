d7de7303c416cc6cd39e83351467b215c90e2931
Alexandre Anicio <aa@tsl.io>
2022-07-02 20:57:09 -0300

    feat: remove dynamic host

--- apps/spotify-app-mf-shell/src/Routes.js
+++ apps/spotify-app-mf-shell/src/Routes.js
@@ -3,52 +3,22 @@ import {Switch, Route} from 'react-router-dom'
 
 import Home from './pages/Home'
 import NotFound from './pages/NotFound'
-import {loadComponent} from './utils/loadComponent'
+const Search = React.lazy(() => import("Search/Search"));
+const Album = React.lazy(() => import("Album/Album"));
 
-const renderMFE = ({remote, url, module}) => {
-  const Component = React.lazy(
-    loadComponent({remote, sharedScope: 'default', module, url}),
-  )
-
-  return (
-    <React.Suspense fallback="Loading...">
-      <Component />
-    </React.Suspense>
+const renderMFE = (MFE) => {
+  return(
+      <React.Suspense fallback="Loading...">
+          <MFE />
+      </React.Suspense>
   )
 }
-
-const isDev = process.env.NODE_ENV === 'development'
-
-const searchURL = isDev
-  ? 'http://localhost:3011/remoteEntry.js'
-  : 'https://mf-app-search.netlify.app/remoteEntry.js'
-const albumURL = isDev
-  ? 'http://localhost:3012/remoteEntry.js'
-  : 'https://mf-app-album.netlify.app/remoteEntry.js'
 
 const Routes = () => {
   return (
     <Switch>
-      <Route
-        path="/album"
-        render={(_) =>
-          renderMFE({
-            remote: 'Album',
-            url: albumURL,
-            module: './Album',
-          })
-        }
-      />
-      <Route
-        path="/search"
-        render={(_) =>
-          renderMFE({
-            remote: 'Search',
-            url: searchURL,
-            module: './Search',
-          })
-        }
-      />
+      <Route path="/album" render={_ => renderMFE(Album)}/>
+      <Route path="/search" render={_ => renderMFE(Search)}/>
       <Route exact path="/" component={Home} />
       <Route component={NotFound} />
     </Switch>
